7d56c50c5b8acb008a4b3ec9889e9329d1ec7926
Michał Perlak <michalperlak@windowslive.com>
2018-01-14 23:13:26 +0100

    Display user tickets

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -1,2 +1,8 @@
 module TicketsHelper
+  def user_tickets(tickets)
+    if current_user.admin?
+      return tickets
+    end
+    tickets.select {|ticket| ticket.user_id == current_user.id}
+  end
 end
