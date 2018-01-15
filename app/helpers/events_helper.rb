a82a4276cd16ca2e4c3423da3927129676e42eab
Michał Perlak <michalperlak@windowslive.com>
2018-01-15 17:09:51 +0100

    Archive events + add typescript

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,5 +2,17 @@ module EventsHelper
   def format_date(date)
     date.strftime("%H:%M %d/%m/%Y")
   end
+
+  def current_events(events)
+    events.select {|event| !is_archived(event)}
+  end
+
+  def archived_events(events)
+    events.select {|event| is_archived(event)}
+  end
+
+  def is_archived(event)
+    event.event_date.past?
+  end
 end
 
--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -1,6 +1,6 @@
 module TicketsHelper
   def user_tickets(tickets)
-    if current_user.admin?
+    if current_user&.admin?
       return tickets
     end
     tickets.select {|ticket| ticket.user_id == current_user.id}
