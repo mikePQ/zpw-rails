becff77851a83242f8910a9d01eeed090ca0153f
Michał Perlak <michalperlak@windowslive.com>
2018-01-20 12:22:24 +0100

    Add logic for ticket price

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -7,7 +7,27 @@ module TicketsHelper
   end
 
   def ticket_price(event_id)
-    100 #TODO
+    event = Event.find(event_id)
+    date = event.event_date
+    today = Date.today
+
+    if today >= date
+      return 1.2 * event.price_high
+    end
+
+    days = (date - today).to_i
+
+    if days >= 25
+      price = event.price_low
+    elsif days >= 15
+      price = (event.price_low + event.price_high) / 2
+    elsif days >= 7
+      price = (event.price_low + 3 * event.price_high) / 4
+    else
+      price = event.price_high
+    end
+
+    price
   end
 
   def total_price(seats)
@@ -16,7 +36,27 @@ module TicketsHelper
   end
 
   def get_amount_to_return(ticket)
-    ticket.price #TODO
+    event = Event.find(ticket.event_id)
+    date = event.event_date
+    today = Date.today
+
+    if today >= date
+      return 0.4 * ticket.price
+    end
+
+    days = (date - today).to_i
+
+    if days >= 25
+      to_return = 0.9 * ticket.price
+    elsif days >= 15
+      to_return = 0.8 * ticket.price
+    elsif days >= 7
+      to_return = 0.7 * ticket.price
+    else
+      to_return = 0.55 * ticket.price
+    end
+
+    to_return
   end
 
   def not_returned(tickets)
